9635b9c60e0bf84980b145dfdf029d9b65fd78fe
m2ak-dev <mirfozil.work@gmail.com>
2023-10-21 23:21:00 +0200

    just responsive

---
+++ menu.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menuItems = document.querySelector('.menu-items');
+
+    menuIcon.addEventListener('click', function() {
+        // Toggle the class to show/hide the menu items
+        menuItems.classList.toggle('active');
+    });
+});
